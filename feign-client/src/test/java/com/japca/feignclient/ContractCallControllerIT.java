0b08f2593e7abb43d2c31fc7366efc696e51f56a
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-05-07 10:20:58 +0200

    Added docker run integration test

---
+++ feign-client/src/test/java/com/japca/feignclient/ContractCallControllerIT.java
@@ -0,0 +1,44 @@
+package com.japca.feignclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Jakub Krhovják on 5/6/18.
+ */
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@AutoConfigureStubRunner(ids = {"com.japca:receiver-service:+:stubs:8082"}, workOffline = true)
+@DirtiesContext
+public class ContractCallControllerIT {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void simpleRestCallTest() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/restCall")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().string(containsString("/restCall-get")))
+				.andExpect(status().isOk());
+	}
+}
--- feign-client/src/test/java/com/japca/feignclient/FeignClientControllerIT.java
+++ feign-client/src/test/java/com/japca/feignclient/FeignClientControllerIT.java
@@ -1,10 +1,10 @@
 package com.japca.feignclient;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.japca.data.PostData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
@@ -12,33 +12,52 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Created by Jakub Krhovják on 5/6/18.
- */
 
-@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
-@AutoConfigureJsonTesters
+@SpringBootTest(classes = FeignClientApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureStubRunner(ids = {"com.japca:receiver-service:+:stubs:8082"}, workOffline = true)
+@AutoConfigureMockMvc
 @DirtiesContext
-public class ContractCallControllerTest {
+public class FeignClientControllerIT {
+
+	@Autowired
+	private ReceiverClient receiverClient;
 
 	@Autowired
 	private MockMvc mvc;
 
+	private ObjectMapper mapper = new ObjectMapper();
+
 	@Test
-	public void simpleRestCallTest() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/restCall")
+	public void callClientDirectly() throws Exception {
+		receiverClient.receive();
+	}
+
+	@Test
+	public void callWithController() throws Exception {
+		mvc.perform(get("/call")
 				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(content().string(containsString("/restCall-get")))
+				.andExpect(content().string(containsString("/receive-get")))
+				.andExpect(status().isOk());
+
+	}
+
+
+	@Test
+	public void callPostTest() throws Exception {
+		mvc.perform(post("/callPost")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(new PostData("Test name",23))))
+				.andExpect(content().string(containsString("/receive-post")))
 				.andExpect(status().isOk());
 	}
+
+
 }
