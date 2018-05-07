752009a964b23ba0d6123a6357da1c5fc7bf3d9e
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-05-07 22:34:05 +0200

    Basic arqullirian ape finished

--- ribbon-client/src/main/java/com/japca/ribbonclient/entity/Record.java
+++ ribbon-client/src/main/java/com/japca/ribbonclient/entity/Record.java
@@ -1,30 +1,32 @@
 package com.japca.ribbonclient.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 /**
  * Created by Jakub krhovják on 4/8/18.
  */
 
 @Entity
+@EqualsAndHashCode(of = "id", doNotUseGetters =true)
 public class Record {
 
 	@Id
-	@GenericGenerator( name = "record_id_seq_generator",
-			strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-			parameters = {
-					@Parameter(name = "sequence_name", value = "record_id_seq"),
-					@Parameter(name = "initial_value", value = "1"),
-					@Parameter(name = "increment_size", value = "1")
-			})
-	@GeneratedValue(generator = "record_id_seq_generator")
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "record_id_seq")
-//	@SequenceGenerator(allocationSize = 1, name = "record_id_seq", sequenceName = "record_id_seq")
+//	@GenericGenerator( name = "record_id_seq_generator",
+//			strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+//			parameters = {
+//					@Parameter(name = "sequence_name", value = "record_id_seq"),
+//					@Parameter(name = "initial_value", value = "1"),
+//					@Parameter(name = "increment_size", value = "1")
+//			})
+//	@GeneratedValue(generator = "record_id_seq_generator")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "record_id_seq")
+	@SequenceGenerator(allocationSize = 1, name = "record_id_seq", sequenceName = "record_id_seq")
 	private Long id;
 
 	private String name;
--- ribbon-client/src/test/java/com/japca/ribbonclient/service/ArquillianApeRecordServiceTest.java
+++ ribbon-client/src/test/java/com/japca/ribbonclient/service/ArquillianApeRecordServiceTest.java
@@ -1,6 +1,5 @@
 package com.japca.ribbonclient.service;
 
-import com.japca.ribbonclient.entity.Record;
 import lombok.extern.slf4j.Slf4j;
 import org.arquillian.ape.junit.rule.ArquillianPersistenceRule;
 import org.arquillian.ape.rdbms.core.RdbmsPopulator;
@@ -15,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Jakub Krhovják on 5/7/18.
@@ -35,28 +34,25 @@ public class ArquillianApeRecordServiceTest {
 	@Autowired
 	private RecordService recordService;
 
+
 	@Before
 	public void setUp() throws Exception {
 		rdbmsPopulator.forUri("jdbc:postgresql://172.17.0.2:5432/postgres")
-				.withDriver(org.postgresql.Driver.class)
-				.withUsername("postgres")
-				.withPassword("postgres")
+				.fromSpringBootConfiguration()
 				.usingDataSet("test-data.yml")
 				.execute();
 	}
 
 	@Test
 	public void name() {
-		List<Record> records = recordService.getRecords();
-		log.info("---------------------------------------------------------------Records: ", records);
+		assertThat(recordService.getRecords()).hasSize(1);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		rdbmsPopulator.forUri("jdbc:postgresql://172.17.0.2:5432/postgres")
-				.withDriver(org.postgresql.Driver.class)
-				.withUsername("postgres")
-				.withPassword("postgres")
+				.fromSpringBootConfiguration()
+				.usingDataSet("test-data.yml")
 				.clean();
 	}
 }
