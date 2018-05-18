e68659228f5fc9189dc676ff8841efbec6180073
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-05-18 13:20:18 +0200

    Removed mongo

--- mongo/src/main/java/com/japca/mongo/MongoDao.java
+++
@@ -1,9 +0,0 @@
-package com.japca.mongo;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by Jakub Krhovják on 5/10/18.
- */
-public interface MongoDao extends CrudRepository<Record, String> {
-}
